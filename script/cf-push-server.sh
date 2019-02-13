b42a8c50545fbe5f6da1d4eb012cb47205fe60ac
Stephan Klevenz <stephan.klevenz@sap.com>
2019-02-13 18:56:19 +0100

    refactor flags and environemnt variables

--- script/cf-push-server.sh
+++ script/cf-push-server.sh
@@ -1,16 +1,28 @@
 #!/usr/bin/env bash
 
 VERSION=$(git log --format="%H" -n 1)
+USAGE="Usage: cf-push-server.sh [s|db]"
+
+if [[ $# -eq 0 ]] ; then
+    echo $USAGE
+    exit 0
+fi
+
+case "$1" in
+    "s") MODE="simulator" ;;
+    "db") MODE="mongodb" ;;
+    *) echo $USAGE; exit 0 ;;
+esac
 
 if [[ $(git diff --stat) != '' ]]; then
   VERSION="$VERSION-dirty"
 fi
 
+echo "=== mode: $MODE";
+echo "=== version: $VERSION";
 
 pushd cfrs-server
   mkdir -p gen
-  sed -e "s/{{VERSION}}/$VERSION/" manifest.yml.template > ./gen/manifest.yml
-
+  sed -e "s/{{VERSION}}/$VERSION/" -e "s/{{MODE}}/$MODE/" manifest.yml.template > ./gen/manifest.yml
   cf push -f ./gen/manifest.yml
-
 popd
